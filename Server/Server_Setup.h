647c4c193a3b1dda22f20247ae44328512a897b1
Mensh <Mensh@DESKTOP-MAC1L97>
2020-07-09 09:22:36 -0400

    quick fix

--- Server/Server_Setup.h
+++ Server/Server_Setup.h
@@ -6,15 +6,39 @@
 #include<stdio.h>
 #include<iostream>
 #include<string>
-#include "Info_Label.h"
-#include "Structs.h"
 
 #pragma comment(lib,"ws2_32.lib")
-#define PORT 8888
+#define PORT 8080
 #define DATA_BUFSIZE 8888
 #define FD_SETSIZE 10
 
 
+typedef struct _SOCKET_INFORMATION
+{
+	CHAR Buffer[DATA_BUFSIZE];
+	WSABUF DataBuf;
+	SOCKET Socket;
+	SOCKET Connected_Socket;
+	OVERLAPPED Overlapp;
+	DWORD BytesSEND;
+	DWORD BytesRECV;
+	bool Read;
+	std::string Name;
+
+	DWORD num;
+
+}Socket_Information, * LPSOCKET_INFORMATION;
+
+typedef struct _Socket_Setup
+{
+	SOCKET Listen_Socket;
+	SOCKET Accept_Socket;
+	FD_SET ReadSet;
+	FD_SET WriteSet;
+	DWORD Total_Sockets;
+	LPSOCKET_INFORMATION SocketArray[10];
+	DWORD Total_fd;
+}Setup_Info, * LPSocket_Setup;
 bool Start_Winsock();
 bool Server_Socket_setup(LPSocket_Setup Setup_Information, SOCKADDR_IN& InternetAddr);
 bool Set_Read_Write(LPSocket_Setup S_Info);
